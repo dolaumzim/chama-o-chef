1b9a2aff1c9b21771a7434ca3076e50a2ca809f9
armando.assini <arm.assini@gmail.com>
2023-09-21 22:29:01 -0300

    Configs, estilização de input e acertos api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiChef = axios.create({
-  baseURL: import.meta.env.URL_API,
+  baseURL: import.meta.env.VITE_URL_API,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
